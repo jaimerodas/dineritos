8023a3281174891c366a02013964f8401fa74f36
Jaime Rodas <jaime@rodas.mx>
2019-05-26 11:23:04 -0500

    Gestiona Cuentas

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,25 @@
+class AccountsController < ApplicationController
+  def index
+    @accounts = Account.order(active: :desc, id: :asc)
+  end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to accounts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :currency)
+  end
+end
