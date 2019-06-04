8c9f4a2f371e652f65e0be9c35d9838982554962
Jaime <jaime@resuelve.mx>
2019-06-04 10:02:38 -0500

    Localiza rutas
    Accounts -> Cuentas
    Balances -> Saldos
    Login -> Ingresar
    
    ../new -> nuevo
    ../edit -> editar
    
    /balances -> /historial
    /create_session/... -> /iniciar_sesion/...

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root "dashboards#show"
 
-  resources :accounts, only: %i[index new create show]
-  resources :balances, param: :date
-  resource :login, only: [:show, :create]
-  get "/create_session/:token", to: "sessions#create", as: "create_session"
+  scope path_names: {new: "nuevo", edit: "editar"} do
+    resources :accounts, path: "cuentas", only: %i[index new create show]
+    resources :balances, path: "saldos", param: :date
+    resource :login, path: "ingresar", only: [:show, :create]
+  end
+  
+  # Rutas directas
+  get "/historial", to: "balances#index", as: "historic"
+  get "/iniciar_sesion/:token", to: "sessions#create", as: "create_session"
 end
