f45ea27e331825e6edd2377cca741d56e887e539
Jaime <jaime@rodas.mx>
2019-10-07 12:32:35 -0500

    Agrega seeds para levantar dev más rápido

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# First we need some accounts:
+accounts = Account.create([
+  {name: "BBVA"}, {name: "BBVA Dólares", currency: "USD"},
+])
+
+# Then we need some dates
+dates = BalanceDate.create([
+  {date: 8.weeks.ago}, {date: 6.weeks.ago},
+  {date: 4.weeks.ago}, {date: 2.weeks.ago},
+  {date: Date.current},
+])
+
+# Finally we create some balances and totals
+dates.each do |date|
+  accounts.each do |account|
+    date.balances.create(account: account, amount: rand(1000..10000))
+  end
+  CalculateTotal.from(date)
+end
