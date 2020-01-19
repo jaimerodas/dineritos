976c8930eb81858465ca923d28cc43acd7246e2b
Jaime Rodas <jaime@rodas.mx>
2020-01-19 07:19:49 -0600

    Renombra cosas de javascript para editar cuentas

--- app/javascript/controllers/edit_account_controller.js
+++ app/javascript/controllers/edit_account_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "stimulus"
+import graph from "graphs"
 
 export default class extends Controller {
   static targets = [ "type", "currency", "apiKey", "apiSecret", "username", "password" ]
