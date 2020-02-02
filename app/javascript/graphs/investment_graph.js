59996e6d42382261d2aa1bc4f9cc425fa8feebb6
Jaime Rodas <jaime@rodas.mx>
2020-02-02 08:42:28 -0600

    Mejora gráfica de inversiones

--- app/javascript/graphs/investment_graph.js
+++ app/javascript/graphs/investment_graph.js
@@ -7,7 +7,9 @@ class InvestmentGraph {
     this.container = element
     this.data = data
     this.margin = ({top: 20, right: 5, bottom: 30, left: 32})
-    this.width = this.container.offsetWidth + 32;
+    this.width = this.container.offsetWidth + 32
+
+    this.setLocale()
 
     this.svg = d3.select(this.container).append("svg")
       .attr("viewBox", [0, 0, this.width, this.height])
@@ -24,45 +26,39 @@ class InvestmentGraph {
       return {date: new Date(d.data.date), value: d[1]}
     })
 
-    this.setLocale()
-  }
-
-  draw() {
-    const x = d3.scaleUtc()
+    this.x = d3.scaleUtc()
       .domain(d3.extent(this.data, d => new Date(d.date)))
       .range([this.margin.left, this.width - this.margin.right])
 
-    const y = d3.scaleLinear()
+    this.y = d3.scaleLinear()
       .domain([0, d3.max(this.series, d => d3.max(d, d => d[1]))]).nice()
       .range([this.height - this.margin.bottom, this.margin.top])
 
+    this.area = d3.area()
+      .curve(d3.curveLinear)
+      .x(d => this.x(new Date(d.data.date)))
+      .y0(d => this.y(d[0]))
+      .y1(d => this.y(d[1]))
+  }
+
+  draw() {
     const xAxis = (g) => g
       .attr("transform", `translate(0,${this.height - this.margin.bottom})`)
-      .call(d3.axisBottom(x).ticks(this.width / 80).tickSizeOuter(0))
+      .call(d3.axisBottom(this.x).ticks(this.width / 80).tickSizeOuter(0))
 
     const yAxis = (g) => g
       .attr("transform", `translate(${this.margin.left},0)`)
-      .call(d3.axisLeft(y).tickFormat(d3.format(".3~s")))
+      .call(d3.axisLeft(this.y).tickFormat(d3.format(".3~s")))
       .call(g => g.selectAll(".tick line").clone()
-          .attr("stroke-opacity", d => d === 0 ? null : 0.2)
-          .attr("stroke-dasharray", "4, 4")
-          .attr("x2", this.width - this.margin.left - this.margin.right))
-
-    const area = d3.area()
-      .curve(d3.curveLinear)
-      .x(d => x(new Date(d.data.date)))
-      .y0(d => y(d[0]))
-      .y1(d => y(d[1]))
+        .attr("stroke-opacity", d => d === 0 ? null : 0.2)
+        .attr("stroke-dasharray", "4, 4")
+        .attr("x2", this.width - this.margin.left - this.margin.right))
+      .call(g => g.select(".domain").remove())
 
     const drawTooltip = () => {
       d3.event.preventDefault()
-      const mouse = d3.mouse(d3.event.target)
-      var [
-        xCoord,
-        yCoord,
-      ] = mouse
-
-      const mouseDate = x.invert(xCoord)
+      var [xCoord, yCoord] = d3.mouse(d3.event.target)
+      const mouseDate = this.x.invert(xCoord)
 
       if (xCoord > this.width - this.margin.right) {
         xCoord = this.width - this.margin.right
@@ -71,18 +67,17 @@ class InvestmentGraph {
       const bisectDate = d3.bisector(d => d.date).left
       const xIndex = bisectDate(this.totals, mouseDate, 1)
       const mousePopulation = this.totals[xIndex - 1].value
-      const mouseDateSnap = x(this.totals[xIndex - 1].date)
+      const mouseDateSnap = this.x(this.totals[xIndex - 1].date)
 
       this.svg.selectAll('.hoverLine')
         .attr('x1', mouseDateSnap)
         .attr('y1', this.margin.top)
         .attr('x2', mouseDateSnap)
         .attr('y2', this.height - this.margin.bottom)
-        .attr('stroke', '#555')
 
-      const isLessThanHalf = mouseDateSnap > this.width / 2;
-      const hoverTextX = isLessThanHalf ? '-0.25em' : '0.25em';
-      const hoverTextAnchor = isLessThanHalf ? 'end' : 'start';
+      const isLessThanHalf = mouseDateSnap > this.width / 2
+      const hoverTextX = isLessThanHalf ? '-0.25em' : '0.25em'
+      const hoverTextAnchor = isLessThanHalf ? 'end' : 'start'
 
       this.svg.selectAll('.hoverText')
         .attr('x', mouseDateSnap)
@@ -90,7 +85,7 @@ class InvestmentGraph {
         .attr('dx', hoverTextX)
         .attr('dy', '.75em')
         .style('text-anchor', hoverTextAnchor)
-        .text(d3.format("$,.2f")(mousePopulation));
+        .text(d3.format("$,.2f")(mousePopulation))
     }
 
     this.svg.append("g")
@@ -98,12 +93,12 @@ class InvestmentGraph {
       .data(this.series)
       .join("path")
         .attr("fill", ({key}) => this.color(key))
-        .attr("d", area)
+        .attr("d", this.area)
       .append("title")
         .text(({key}) => key)
 
-    this.svg.append("g").call(xAxis)
-    this.svg.append("g").call(yAxis)
+    this.svg.append("g").attr("class", "axis").call(xAxis).attr("font-family", null)
+    this.svg.append("g").attr("class", "axis").call(yAxis).attr("font-family", null)
 
     this.svg.append('line').classed('hoverLine', true)
     this.svg.append("text").classed('hoverText', true)
@@ -114,9 +109,9 @@ class InvestmentGraph {
       .attr('y', 0)
       .attr('width', this.width)
       .attr('height', this.height)
-    ;
 
-    this.svg.on('mousemove', drawTooltip);
+
+    this.svg.on('mousemove', drawTooltip)
   }
 
   interpolateColors(dataLength, colorScale) {
@@ -129,7 +124,7 @@ class InvestmentGraph {
       colorArray.push(colorScale(colorPoint))
     }
 
-    return colorArray;
+    return colorArray
   }
 
   setLocale() {
@@ -140,7 +135,7 @@ class InvestmentGraph {
       periods: ["AM", "PM"],
       days: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
       shortDays: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
-      months: ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+      months: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
       shortMonths: ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
     })
   }
