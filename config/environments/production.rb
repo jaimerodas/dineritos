f0c51f31f93749488108beb8e6ec7ac98afe7712
Jaime Rodas <jaime@rodas.mx>
2020-12-17 21:53:26 -0600

    Regresa las configuraciones de dominio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dineritos_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = {api_token: Rails.application.credentials[:postmark]}
+  config.action_mailer.default_url_options = {host: "dineritos.mx"}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
