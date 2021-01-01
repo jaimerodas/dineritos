3b160d395a8cb60560a060a917376670461a6485
Jaime Rodas <jaime@rodas.mx>
2021-01-01 12:25:05 -0600

    Rediseñamos la función de currency
    Tenía dos funciones, una `diff` y una `currency` que estaban encargadas
    de lo mismo. Una regresaba dinero formateado y la otra le agregaba
    clases y símbolos. Las unifiqué en currency y le agregué una opción
    opcional de `diff: true|false` para agregar el resto de los parámetros.

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -1,19 +1,9 @@
 module MoneyHelper
-  def diff(number, unit: "")
-    classes = "diff"
-    return tag.span("-", class: classes += " zero") if !number || number.zero?
+  def currency(number, diff: false)
+    return tag.span("-", class: "zero") if !number || number.zero?
+    text = number_to_currency(number, unit: "")
 
-    text = currency(number, unit: unit)
-
-    if number.zero? then classes += " zero"
-    elsif number.negative? then classes += " neg"
-    else text = "+#{text}"
-    end
-
-    tag.span text, class: classes
-  end
-
-  def currency(number, unit: "")
-    number_to_currency(number, unit: unit)
+    return tag.span(text) unless diff
+    number.negative? ? tag.span(text, class: "diff neg") : tag.span("+#{text}", class: "diff")
   end
 end
