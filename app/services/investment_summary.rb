17be45ecc6cb4ef196e060263f896350c006e824
Jaime Rodas <jaime@rodas.mx>
2022-01-24 09:57:01 -0600

    Arregla iterador de fechas
    Resulta que antes, Ruby/Rails me dejaba hacer:
    
    (1.year.ago..Date.current).include?(Date.current)
    
    Aunque `1.year.ago` no es una fecha! Resulta que es un Time. En
    alguna de las actualizaciones recientes se ha de haber quitado esa
    posibilidad y empezó a sacar error. Para resolverlo, bastó agregar
    un simple `.to_date` a `1.year.ago` y ya regresa a ser un rango
    válido.

--- app/services/investment_summary.rb
+++ app/services/investment_summary.rb
@@ -60,7 +60,7 @@ class InvestmentSummary
   private
 
   def calculate_period_from(year)
-    return 1.year.ago..Date.current if year == "past_year"
+    return 1.year.ago.to_date..Date.current if year == "past_year"
     return earliest_date..Date.current if year == "all"
     year = year.to_i if year.instance_of?(String)
     Date.new(year)...Date.new(year + 1)
