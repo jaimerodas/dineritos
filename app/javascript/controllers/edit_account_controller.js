44b2ea167f9d0257563a3ca0366b70d4e3151f28
Jaime Rodas <jaime@rodas.mx>
2023-10-24 18:23:00 -0600

    Simplifica edit_account_controller.js
    Me compliqué la vida muchísimo al hacer el controlador de editar cuentas. Ahora que estaba agregando el scraper de Apify vi la luz y me di cuenta que el mismo funcionamiento lo podía lograr con mucho menos código al mismo tiempo que mejoraría la legibilidad.

--- app/javascript/controllers/edit_account_controller.js
+++ app/javascript/controllers/edit_account_controller.js
@@ -1,76 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [
-    "type", "currency", "apiKey", "apiSecret", "username", "password", "issuer", "secret",
-    "rgtoken", "rgpassword", "rgusername", "apusername", "appassword", "aptoken", "apactor"
-  ]
+  static targets = ["type"]
   connect() {
     this.change()
   }
 
   change() {
-    var fields = new Array
+    const selected_platform = this.typeTarget.value
+    const platforms = ["afluenta", "apify", "bitso", "no_platform"]
+    const fields = ["afluenta", "apify", "bitso", "credentials", "no_platform"]
+    const fields_to_show = platforms.includes(selected_platform) ? selected_platform : "credentials"
 
-    switch (this.typeTarget.value) {
-      case "bitso":
-        fields.push("bitso")
-        break
-      case "no_platform":
-        fields.push("regular")
-        break
-      case "afluenta":
-        fields.push("twoFactor")
-      case "red_girasol":
-        fields.push("redGirasol")
-        break
-      case "apify":
-        fields.push("apify")
-        break
-      default:
-        fields.push("credential")
+    fields.forEach(e => this.elementVisible(e + "_fields", e === fields_to_show))
+
+    if (selected_platform !== "no_platform") {
+      document.getElementById("account_currency").value = "MXN"
     }
-    [
-      "bitso","regular","credential","afluenta", "redGirasol", "apify"
-    ].forEach(e => this[e + "FieldsVisible"](fields.includes(e)))
-  }
-
-  regularFieldsVisible(visible) {
-    this.elementVisible("account_currency_field", visible)
-  }
-
-  bitsoFieldsVisible(visible) {
-    this.elementVisible("bitso_account_settings_field", visible)
-    this.hideCurrencyIfVisible(visible)
-  }
-
-  redGirasolFieldsVisible(visible) {
-    this.elementVisible("red_girasol_field", visible)
-    this.hideCurrencyIfVisible(visible)
-  }
-
-  credentialFieldsVisible(visible) {
-    this.elementVisible("account_settings_credentials_field", visible)
-    this.hideCurrencyIfVisible(visible)
-  }
-
-  afluentaFieldsVisible(visible) {
-    this.elementVisible("afluenta_field", visible)
-  }
-
-  apifyFieldsVisible(visible) {
-    this.hideCurrencyIfVisible(visible)
-    this.elementVisible("apify_field", visible)
   }
 
   elementVisible(name, visible) {
     var display = visible ? "block" : "none"
     document.getElementById(name).style.display = display
     document.querySelectorAll(`#${name} input, #${name} textarea`).forEach(e => e.disabled = !visible)
-  }
-
-  hideCurrencyIfVisible(visible) {
-    if (visible) { return }
-    document.getElementById("account_currency_field").value = "MXN"
   }
 }
