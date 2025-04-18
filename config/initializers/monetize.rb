58e7978e3447291db08b8119c750c7d997ae6829
Jaime Rodas <jaime@rodas.mx>
2025-04-18 06:05:23 -0600

    Arregla deprecation warnings
    `money-rails` y `webauthn` estaban levantando warnings por pequeñas configuraciones que van a cambiar en el futuro.

--- config/initializers/monetize.rb
+++ config/initializers/monetize.rb
@@ -1,1 +1,2 @@
 Money.default_currency = "USD"
+Money.rounding_mode = BigDecimal::ROUND_HALF_EVEN
--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,7 +1,7 @@
 WebAuthn.configure do |config|
   # This value needs to match `window.location.origin` evaluated by
   # the User Agent during registration and authentication ceremonies.
-  config.origin = (Rails.env == "development") ? "http://localhost:3000" : ENV["WEBAUTHN_HOST"]
+  config.allowed_origins = (Rails.env == "development") ? ["http://localhost:3000"] : [ENV["WEBAUTHN_HOST"]]
 
   # Relying Party name for display purposes
   config.rp_name = "Dineritos"
