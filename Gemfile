749081bcf41da4ab42c7a94cc8bc4431edb96df1
Jaime Rodas <jaime@rodas.mx>
2025-05-10 11:41:25 -0600

    Agrega simplecov

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ group :development do
   # gem "i18n-debug"
 end
 
+group :test do
+  gem "simplecov"
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,10 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require "simplecov"
+SimpleCov.start "rails" do
+  add_group "Services", "app/services"
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
