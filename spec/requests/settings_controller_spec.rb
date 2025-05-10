5a1bd0f9e814a5831b8fa6d820f4b145fdaa2fc7
Jaime Rodas <jaime@rodas.mx>
2025-05-10 11:44:58 -0600

    Agrega pruebas de settings_controller

---
+++ spec/requests/settings_controller_spec.rb
@@ -0,0 +1,85 @@
+require "rails_helper"
+
+RSpec.describe SettingsController, type: :request do
+  let(:user) { User.create(email: "test@example.com") }
+
+  describe "#show" do
+    context "when user is logged in" do
+      before do
+        # Set session user_id directly to simulate being logged in
+        allow_any_instance_of(SettingsController).to receive(:current_user).and_return(user)
+        get settings_path
+      end
+
+      it "returns a successful response" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns HTML content" do
+        expect(response.content_type).to include("text/html")
+      end
+
+      # We're removing the assigns and render_template tests since they require
+      # the rails-controller-testing gem which isn't included
+    end
+
+    context "when user is not logged in" do
+      before do
+        get settings_path
+      end
+
+      it "redirects to login page" do
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+  describe "#create" do
+    context "when user is logged in" do
+      before do
+        # Set session user_id directly to simulate being logged in
+        allow_any_instance_of(SettingsController).to receive(:current_user).and_return(user)
+      end
+
+      it "updates user settings" do
+        expect {
+          post settings_path, params: {settings: {daily_email: "1", send_email_after_update: "0"}}
+        }.to change { user.reload.settings }.to({"daily_email" => true, "send_email_after_update" => false})
+      end
+
+      it "redirects to settings page" do
+        post settings_path, params: {settings: {daily_email: "1"}}
+        expect(response).to redirect_to(settings_path)
+      end
+
+      it "sets a success flash message" do
+        post settings_path, params: {settings: {daily_email: "1"}}
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        post settings_path, params: {settings: {daily_email: "1"}}
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+  describe "#settings_to_b" do
+    before do
+      # Set session user_id directly to simulate being logged in
+      allow_any_instance_of(SettingsController).to receive(:current_user).and_return(user)
+    end
+
+    it "converts string '1' values to true" do
+      post settings_path, params: {settings: {daily_email: "1", send_email_after_update: "1"}}
+      expect(user.reload.settings).to eq({"daily_email" => true, "send_email_after_update" => true})
+    end
+
+    it "converts string '0' values to false" do
+      post settings_path, params: {settings: {daily_email: "0", send_email_after_update: "0"}}
+      expect(user.reload.settings).to eq({"daily_email" => false, "send_email_after_update" => false})
+    end
+  end
+end
