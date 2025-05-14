54df10beb5948e169c0b0891a179589d0fac92b2
Jaime Rodas <jaime@rodas.mx>
2025-05-14 07:05:33 -0600

    Agrega pruebas para AccountBalancesController
    Habiendo probado el controlador, aproveché para hacer algunas mejoras y hacerlo más DRY y más acorde a convención.

--- app/controllers/account_balances_controller.rb
+++ app/controllers/account_balances_controller.rb
@@ -1,23 +1,23 @@
 class AccountBalancesController < ApplicationController
   before_action :auth
-  before_action :account_balance, only: %i[edit update]
+  before_action :set_account, only: %i[new create]
+  before_action :set_balance, only: %i[edit update]
 
   def new
     @balance = Balance.new(
       date: Date.current,
-      account: account,
-      amount: account.last_amount.amount,
-      currency: account.currency
+      account: @account,
+      amount: @account.last_amount.amount,
+      currency: @account.currency
     )
   end
 
   def create
     @balance = current_user.balances.find_or_initialize_by(
-      date: Date.current, account: account, currency: account.currency
+      date: Date.current, account: @account, currency: @account.currency
     )
 
-    if @balance.update(account_balance_params.merge(validated: true))
-      ServicesMailer.new_daily_update(current_user).deliver_now if user_wants_to_be_notified?
+    if UpdateBalance.run(balance: @balance, params: account_balance_params)
       redirect_to account_movements_path(@balance.account)
     else
       render :new
@@ -37,19 +37,15 @@ class AccountBalancesController < ApplicationController
 
   private
 
-  def account
-    @account ||= current_user.accounts.find(params[:account_id])
+  def set_account
+    @account = current_user.accounts.find(params[:account_id])
   end
 
-  def account_balance
+  def set_balance
     @balance = current_user.balances.find(params[:id])
   end
 
   def account_balance_params
     params.require(:balance).permit(:amount, :transfers)
-  end
-
-  def user_wants_to_be_notified?
-    current_user.settings && current_user.settings["send_email_after_update"]
   end
 end
---
+++ spec/requests/account_balances_controller_spec.rb
@@ -0,0 +1,131 @@
+require "rails_helper"
+
+RSpec.describe AccountBalancesController, type: :request do
+  fixtures :users, :accounts, :balances
+
+  let(:user) { users(:test_user) }
+  let(:account) { accounts(:savings_account) }
+  let(:balance) { balances(:savings_jan1) }
+
+  describe "#new" do
+    context "when user is logged in" do
+      before do
+        allow_any_instance_of(AccountBalancesController).to receive(:current_user).and_return(user)
+        travel_to Date.new(2023, 1, 1)
+      end
+
+      after { travel_back }
+
+      it "returns a successful response" do
+        get new_account_account_balance_path(account)
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns HTML content" do
+        get new_account_account_balance_path(account)
+        expect(response.content_type).to include("text/html")
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        get new_account_account_balance_path(account)
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+  describe "#create" do
+    before do
+      allow_any_instance_of(AccountBalancesController).to receive(:current_user).and_return(user)
+      travel_to Date.new(2023, 1, 1)
+    end
+
+    after { travel_back }
+
+    context "with valid parameters" do
+      it "creates or updates the balance and redirects to movements path" do
+        post account_account_balances_path(account), params: {balance: {amount: "6000", transfers: "100"}}
+        expect(response).to redirect_to(account_movements_path(account))
+        new_balance = Balance.find_by(account: account, date: Date.new(2023, 1, 1))
+        expect(new_balance.amount_cents).to eq(6000 * 100)
+        expect(new_balance.transfers_cents).to eq(100 * 100)
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders the new template" do
+        post account_account_balances_path(account), params: {balance: {amount: "", transfers: ""}}
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to include("text/html")
+      end
+    end
+  end
+
+  describe "#edit" do
+    context "when user is logged in" do
+      before do
+        allow_any_instance_of(AccountBalancesController).to receive(:current_user).and_return(user)
+      end
+
+      it "returns a successful response" do
+        get edit_account_balance_path(balance)
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns HTML content" do
+        get edit_account_balance_path(balance)
+        expect(response.content_type).to include("text/html")
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        get edit_account_balance_path(balance)
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+
+  describe "#update" do
+    let(:update_params) { {balance: {amount: "7000", transfers: "200"}} }
+
+    context "when user is logged in" do
+      before do
+        allow_any_instance_of(AccountBalancesController).to receive(:current_user).and_return(user)
+      end
+
+      context "when the update is successful" do
+        before do
+          allow(UpdateBalance).to receive(:run).and_return(true)
+        end
+
+        it "redirects to movements path for the balance's account and month" do
+          patch account_balance_path(balance), params: update_params
+          expect(response).to redirect_to(
+            account_movements_path(balance.account, month: balance.date.strftime("%Y-%m"))
+          )
+        end
+      end
+
+      context "when the update fails" do
+        before do
+          allow(UpdateBalance).to receive(:run).and_return(false)
+        end
+
+        it "renders the edit template" do
+          patch account_balance_path(balance), params: update_params
+          expect(response).to have_http_status(:success)
+          expect(response.content_type).to include("text/html")
+        end
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        patch account_balance_path(balance), params: update_params
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+end
