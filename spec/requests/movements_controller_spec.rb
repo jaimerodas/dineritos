a62858f45aec9b83e1648d25a9b7f228612f6f81
Jaime Rodas <jaime@rodas.mx>
2025-05-14 07:42:47 -0600

    Agrega pruebas para MovementsController

---
+++ spec/requests/movements_controller_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe MovementsController, type: :request do
+  fixtures :users, :accounts
+
+  let(:user) { users(:test_user) }
+  let(:account) { accounts(:savings_account) }
+
+  describe "#index" do
+    context "when user is logged in" do
+      before do
+        allow_any_instance_of(MovementsController)
+          .to receive(:current_user).and_return(user)
+        travel_to Date.new(2023, 1, 1)
+      end
+
+      after { travel_back }
+
+      it "returns a successful HTML response" do
+        get account_movements_path(account)
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to include("text/html")
+      end
+
+      it "handles month parameter" do
+        get account_movements_path(account), params: {month: "2023-02"}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        get account_movements_path(account)
+        expect(response).to redirect_to(login_path)
+      end
+    end
+  end
+end
