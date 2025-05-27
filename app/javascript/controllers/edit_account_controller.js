802581bff914b46577761ad55bec47beabd26d51
Jaime Rodas <jaime@rodas.mx>
2025-05-27 12:00:11 -0600

    Arregla typo

--- app/javascript/controllers/edit_account_controller.js
+++ app/javascript/controllers/edit_account_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
   change() {
     const selected_platform = this.typeTarget.value
     const platforms = ["apify", "afluenta", "bitso", "no_platform"]
-    const fields = ["apify", "afluenta" "bitso", "credentials", "no_platform"]
+    const fields = ["apify", "afluenta", "bitso", "credentials", "no_platform"]
     const fields_to_show = platforms.includes(selected_platform) ? selected_platform : "credentials"
 
     fields.forEach(e => this.elementVisible(e + "_fields", e === fields_to_show))
